ecf009dc61f3af4d33ff0cba06d09eaf8611a781
Kum Yew <lkyew@live.com.sg>
2024-05-08 14:31:15 +0800

    First Commit, test scipt w json test cases

---
+++ Variables-and-Simple-Data-Types-String/Exercise-1-Input-and-Output/main.py
@@ -0,0 +1,7 @@
+# Write your code below here. You should be using the print() function to complete this exercise
+# In this exercise, you will be using input() to get user input
+# and string formatting to output a formatted string with print()
+
+# name = input()
+# print("Hello " + name)
+print("Hello " + input("What is your name? "))
